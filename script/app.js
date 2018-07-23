0b2c6e0dcbc6c20faeb6f2c0c33c4d43de07df14
Damian <37978110+dszyszek@users.noreply.github.com>
2018-07-23 21:32:21 +0200

    add beginning functions

--- script/app.js
+++ script/app.js
@@ -1,1 +1,17 @@
-alert('works');
+new Vue({
+    el: '.main',
+    data: {
+        playerHealth: 0,
+        monsterHealth: 0,
+        isActive: false,
+        logActive: false
+    },
+    methods: {
+        startGame: function(){
+            this.isActive = !this.isActive;
+            //console.log(this.isActive);
+            this.monsterHealth = 100;
+            this.playerHealth = 100;
+        }
+    }
+})
