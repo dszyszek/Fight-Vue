d313fe22257a220190ab0035cfb59a722350d390
Damian <37978110+dszyszek@users.noreply.github.com>
2018-07-23 23:36:00 +0200

    add functionality to actionButtons

--- script/app.js
+++ script/app.js
@@ -1,8 +1,8 @@
 new Vue({
     el: '.main',
     data: {
-        playerHealth: 0,
-        monsterHealth: 0,
+        playerHealth: '',
+        monsterHealth: '',
         isActive: false,
         logActive: false
     },
@@ -12,6 +12,22 @@ new Vue({
             //console.log(this.isActive);
             this.monsterHealth = 100;
             this.playerHealth = 100;
+        },
+        attack: function(minAtt, maxAtt, who){
+                if(this.monsterHealth <= 0 || this.playerHealth <= 0){this.isActive = false;
+                     return}
+                let damage = Math.max(Math.floor(Math.random()*maxAtt),minAtt);
+                if(who == 'monster'){ this.monsterHealth = Math.max((this.monsterHealth-damage),0)}
+                else if(who == 'player'){this.playerHealth = Math.max((this.playerHealth-damage),0)};
+        },
+        heal: function(){
+            if(this.playerHealth >= 90){return this.playerHealth = 100}
+            this.playerHealth += 10;
+            this.monsterHealth += 2;
+            this.attack(3,7,"player");
+        },
+        giveUp: function(){
+            return this.isActive = false
         }
     }
 })
