9cde9fa9117b64c8583268dd212a2f62e697a08e
Damian <37978110+dszyszek@users.noreply.github.com>
2018-07-24 10:36:36 +0200

    add functionality to log

--- script/app.js
+++ script/app.js
@@ -4,12 +4,12 @@ new Vue({
         playerHealth: '',
         monsterHealth: '',
         isActive: false,
-        logActive: false
+        logActive: false,
+        logInfo: []
     },
     methods: {
         startGame: function(){
             this.isActive = !this.isActive;
-            //console.log(this.isActive);
             this.monsterHealth = 100;
             this.playerHealth = 100;
         },
@@ -17,17 +17,32 @@ new Vue({
                 if(this.monsterHealth <= 0 || this.playerHealth <= 0){this.isActive = false;
                      return}
                 let damage = Math.max(Math.floor(Math.random()*maxAtt),minAtt);
+                console.log('here');
                 if(who == 'monster'){ this.monsterHealth = Math.max((this.monsterHealth-damage),0)}
                 else if(who == 'player'){this.playerHealth = Math.max((this.playerHealth-damage),0)};
+                this.logNotifications(`${who} was attacked with ${damage}`);
+             
         },
         heal: function(){
-            if(this.playerHealth >= 90){return this.playerHealth = 100}
+            if(this.playerHealth >= 90){
+                this.logNotifications(`player is now fully healed`);
+                return  this.playerHealth = 100}
             this.playerHealth += 10;
             this.monsterHealth += 2;
-            this.attack(3,7,"player");
+            this.attack(3, 7, 'player');
+            this.logNotifications(`player was healed with 10`);
+            this.logNotifications(`monster was healed with 2`);
+        },
+        logNotifications: function(text){
+            this.logActive = true;
+            this.logInfo.push(text);
         },
         giveUp: function(){
-            return this.isActive = false
+             this.isActive = false;
+             this.logActive = false;
+             this.logInfo = [];
+             return
         }
+       
     }
 })
